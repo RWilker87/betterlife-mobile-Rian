2eef8311a353ee07ffef9e44e0779b365f2c62ed
Daniel Duarte <danielduartefilho.df@gmail.com>
2024-07-09 20:40:22 -0300

    tela de cadastro criada

---
+++ lib/screens/register/register_repository.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import '../../shared/components/dialogs/add_user_dialog.dart';
+import '../../shared/constants/style_constants.dart';
+
+class RegisterRepository {
+  static void signUp(name, cpf, email, password, context) async {
+    final dio = Dio();
+
+    if (name.toString().isEmpty ||
+        cpf.toString().isEmpty ||
+        email.toString().isEmpty ||
+        password.toString().isEmpty) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text('Impossível adicionar usuário'),
+                content: MaterialButton(
+                  onPressed: () => Navigator.pop(context),
+                  color: kDetailColor,
+                  child: const Text(
+                    'Ok!',
+                    style: TextStyle(color: kBackgroundColor),
+                  ),
+                ),
+              ));
+    } else {
+      var response = await dio.post(
+        'baseUrl/users',
+        options: Options(
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+        ),
+        data: {
+          "name": name,
+          "cpf": cpf,
+          "email": email,
+          "password": password,
+        },
+      );
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        showDialog(
+            context: context,
+            builder: (context) =>
+                AddUserDialog(email.toString(), password.toString()));
+      } else {
+        showDialog(
+            context: context,
+            builder: (context) => const AlertDialog(
+                  title: Text('Impossível adicionar usuário'),
+                  content: Text('Ok!'),
+                ));
+      }
+    }
+  }
+}
